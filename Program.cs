f52fafdf6aadba8b233376466bdfe9fe2ad35f6e
danilorezende-code <danilorezende@gmail.com>
2020-04-29 02:27:23 -0300

    Projeto de testes iniciado com Ola mundo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ws_vs_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Teste do GitHub
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
